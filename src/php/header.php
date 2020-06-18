3fecf113bf8c487c142911c213f1df8e444cadd3
NetworkMonk <jamesplant@gmail.com>
2020-06-18 15:58:19 +0100

    Added custom styles for primary and secondary colours

--- src/php/header.php
+++ src/php/header.php
@@ -8,6 +8,34 @@
         <meta charset="<?php bloginfo('charset'); ?>">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <?php wp_head(); ?>
+
+        <?php
+            // Create custom style sheets from theme customisation
+
+            // Get primary and secondary colors
+            $primary_color = get_theme_mod( 'primary_background_color', DEFAULT_PRIMARY_COLOR );
+            $primary_color = strtolower( '#' . ltrim( $primary_color, '#' ) );
+
+            $secondary_color = get_theme_mod( 'secondary_background_color', DEFAULT_SECONDARY_COLOR );
+            $secondary_color = strtolower( '#' . ltrim( $secondary_color, '#' ) );
+        ?>
+        <style>
+            .hover-bg-primary:hover {
+                background-color: <?php echo $primary_color; ?>;
+            }
+
+            .hover-bg-secondary:hover {
+                background-color: <?php echo $secondary_color; ?>;
+            }
+
+            .hover-text-primary:hover {
+                color: <?php echo $primary_color; ?>;
+            }
+
+            .hover-text-secondary:hover {
+                color: <?php echo $secondary_color; ?>;
+            }
+        </style>
     </head>
 
     <body <?php body_class('flex flex-col h-full'); ?>>
